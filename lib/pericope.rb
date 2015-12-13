ca115f5f3b6ffc8c6a5fbec66a67b5953b4ea550
Bob Lail <bob.lailfamily@gmail.com>
2015-12-13 17:13:49 -0600

    [refactor] Moved canonical book names from chapter_verse_count to book_abbreviations (10m)

--- lib/pericope.rb
+++ lib/pericope.rb
@@ -12,13 +12,13 @@ class Pericope
 
 
   def self.book_names
-    load_chapter_verse_count_books! unless defined?(@book_names)
+    load_book_abbreviations! unless defined?(@book_names)
     @book_names
   end
 
   def self.book_name_regexes
-    @book_name_regexes ||= book_abbreviations.
-      map { |book_number, book_regex| [book_number, /\b#{book_regex}\b/] }
+    load_book_abbreviations! unless defined?(@book_name_regexes)
+    @book_name_regexes
   end
 
 
@@ -475,9 +475,8 @@ private
 
 
   def self.load_chapter_verse_count_books!
-    current_book_name = nil
+    current_book_id = nil
     chapters = 0
-    @book_names = []
     @chapter_verse_counts = {}
     @book_chapter_counts = []
 
@@ -485,45 +484,44 @@ private
     File.open(path) do |file|
       file.each do |text|
         row = text.chomp.split("\t")
-        id, verses, book_name = row[0].to_i, row[1].to_i, row[2]
+        id, verses = row[0].to_i, row[1].to_i
+        book_id = get_book(id)
 
         @chapter_verse_counts[id] = verses
 
-        unless current_book_name == book_name
-          if current_book_name
-            @book_names.push current_book_name
-            @book_chapter_counts.push chapters
-          end
-          current_book_name = book_name
+        unless current_book_id == book_id
+          @book_chapter_counts.push chapters if current_book_id
+          current_book_id = book_id
         end
 
         chapters = get_chapter(id)
       end
     end
 
-    @book_names.push current_book_name
     @book_chapter_counts.push chapters
   end
 
 
 
   def self.load_book_abbreviations!
+    @book_names = []
+    @book_name_regexes = {}
+
     path = File.expand_path(File.dirname(__FILE__) + "/../data/book_abbreviations.txt")
-    book_abbreviations = []
     File.open(path) do |file|
       file.each do |text|
-        unless text.start_with?("#") # skip comments
+        next if text.start_with?("#") # skip comments
 
-          # the file contains tab-separated values.
-          # the first value is the ordinal of the book, subsequent values
-          # represent abbreviations and misspellings that should be recognized
-          # as the aforementioned book.
-          segments = text.chomp.split("\t")
-          book_abbreviations << [segments.shift.to_i, "(?:#{segments.join("|")})"]
-        end
+        # the file contains tab-separated values.
+        # the first value is the ordinal of the book, subsequent values
+        # represent abbreviations and misspellings that should be recognized
+        # as the aforementioned book.
+        segments = text.chomp.split("\t")
+        book_id = segments.shift.to_i
+        @book_names[book_id - 1] = segments.shift
+        @book_name_regexes[book_id] = /\b(?:#{segments.join("|")})\b/
       end
     end
-    Hash[book_abbreviations]
   end
 
 
@@ -531,12 +529,6 @@ private
   def self.chapter_verse_counts
     load_chapter_verse_count_books! unless defined?(@chapter_verse_counts)
     @chapter_verse_counts
-  end
-
-
-
-  def self.book_abbreviations
-    @book_abbreviations ||= load_book_abbreviations!
   end
 
 
