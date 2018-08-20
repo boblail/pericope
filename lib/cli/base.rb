b132fa2d77fd0ad94df89daf9854edc8f5f9120d
Bob Lail <bob.lailfamily@gmail.com>
2018-08-20 09:55:22 -0500

    [refactor] Removed excess whitespace (1m)

--- lib/cli/base.rb
+++ lib/cli/base.rb
@@ -1,30 +1,18 @@
 module CLI
   class Base
 
-
-
     def self.command(name, description, &block)
-
     end
-
-
 
     def self.print_commands
       usage = ""
-
     end
 
-
-
   private
-
-
 
     def commands
       @commands || {}
     end
-
-
 
   end
 end
